998becd6462c2a3c9a1eb575b735c2e4d402fbb7
Salehziad <salehziad1999@gmail.com>
2022-07-20 11:49:32 +0300

    D

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 import { Container, Row, Col } from "react-bootstrap";
 // import { MailchimpForm } from "./MailchimpForm";
-import logo from "../assets/img/logo.svg";
+import logo from "../assets/img/logo.png";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
-
+import navIcon4 from '../assets/img/nav-icon4.svg';
 export const Footer = () => {
   return (
     <footer className="footer">
@@ -16,9 +16,10 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+            <a target='_blank' href="https://www.linkedin.com/in/saleh-ziad-6b40a1214" rel="noreferrer"><img src={navIcon1} alt="" /></a>
+              <a target='_blank' href='https://www.facebook.com/profile.php?id=100005955198338' rel="noreferrer"><img src={navIcon2} alt="" /></a>
+              <a target='_blank' href='https://instagram.com/saleh.dalalshh?igshid=YmMyMTA2M2Y=' rel="noreferrer"><img src={navIcon3} alt="" /></a>
+              <a target='_blank' href='https://instagram.com/saleh.dalalshh?igshid=YmMyMTA2M2Y=' rel="noreferrer"><img src={navIcon4} alt="" /></a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
